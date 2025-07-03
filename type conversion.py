72f90910867394ea1cce0fa8732562ac7907a849
poojithayanamadala <poojitha.yanamadala25@gmail.com>
2025-07-03 23:02:33 +0530

    Add files via upload

---
+++ type conversion.py
@@ -0,0 +1,13 @@
+"""
+type conversion:conversion the datatype form one format to other format.
+implicit type conversion:no loss of data(lower datatypes are placing into higher datatypes)
+"""
+d=2345
+print(d)
+print(float(d))
+"""
+exlicit type conversion:loss of data (higher datatypes are placing into lower datatypes)
+"""
+d=3456.6789
+print(d)
+print(int(d))
